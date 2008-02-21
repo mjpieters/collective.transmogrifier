8ce0872c608bf576a022b617435017e48f4c327d
Martijn Pieters <mj@zopatista.com>
2008-02-21 05:55:33 +0000

    Add an optional condition to the inserter section

--- collective/transmogrifier/sections/inserter.py
+++ collective/transmogrifier/sections/inserter.py
@@ -1,7 +1,7 @@
 from zope.interface import classProvides, implements
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
-from collective.transmogrifier.utils import Expression
+from collective.transmogrifier.utils import Expression, Condition
 
 class InserterSection(object):
     classProvides(ISectionBlueprint)
@@ -11,9 +11,14 @@ class InserterSection(object):
         self.key = Expression(options['key'], transmogrifier, name, options)
         self.value = Expression(options['value'], transmogrifier, name,
                                 options)
+        self.condition = options.get('condition')
+        if self.condition is not None:
+            self.condition = Condition(self.condition, transmogrifier, name,
+                                       options)
         self.previous = previous
     
     def __iter__(self):
         for item in self.previous:
-            item[self.key(item)] = self.value(item)
+            if self.condition is None or self.condition(item):
+                item[self.key(item)] = self.value(item)
             yield item
