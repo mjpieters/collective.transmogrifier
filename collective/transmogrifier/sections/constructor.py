78dce1de01f5a35c2dee79765eae8d3df35de3d7
Martijn Pieters <mj@zopatista.com>
2008-04-04 13:25:11 +0000

    Content constructor initialisation
    * Documentation about the options
     * Loading options and defaults

--- collective/transmogrifier/sections/constructor.py
+++ collective/transmogrifier/sections/constructor.py
@@ -1,6 +1,8 @@
 from zope.interface import classProvides, implements
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.utils import Matcher
+from collective.transmogrifier.utils import defaultKeys
 
 class ConstructorSection(object):
     classProvides(ISectionBlueprint)
@@ -8,3 +10,23 @@ class ConstructorSection(object):
     
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
+        
+        if 'id-key' in options:
+            idkeys = options['id-key'].splitlines()
+        else:
+            idkeys = defaultKeys(options['blueprint'], name, 'id')
+            idkeys += ('getId', 'id')
+        self.idkey = Matcher(*idkeys)
+        
+        if 'type-key' in options:
+            typekeys = options['type-key'].splitlines()
+        else:
+            typekeys = defaultKeys(options['blueprint'], name, 'type')
+            typekeys += ('portal_type', 'Type')
+        self.typekey = Matcher(*typekeys)
+        
+        if 'path-key' in options:
+            pathkeys = options['type-key'].splitlines()
+        else:
+            pathkeys = defaultKeys(options['blueprint'], name, 'path')
+        self.pathkey = Matcher(*pathkeys)
