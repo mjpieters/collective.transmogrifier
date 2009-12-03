ace748ad8feed097c7494fba92f4ea074e90e177
Lennart Regebro <regebro@gmail.com>
2009-12-03 21:36:16 +0000

    Added a condition to the manipulator.

--- src/collective/transmogrifier/sections/manipulator.py
+++ src/collective/transmogrifier/sections/manipulator.py
@@ -2,7 +2,7 @@ import copy
 from zope.interface import classProvides, implements
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
-from collective.transmogrifier.utils import Matcher, Expression
+from collective.transmogrifier.utils import Matcher, Expression, Condition
 
 class ManipulatorSection(object):
     classProvides(ISectionBlueprint)
@@ -15,15 +15,18 @@ class ManipulatorSection(object):
             self.dest = Expression(options['destination'], transmogrifier,
                                    name, options)
         self.delete = Matcher(*options.get('delete', '').splitlines())
+        self.condition = Condition(options.get('condition', 'python:True'),
+                                   transmogrifier, name, options)
         self.previous = previous
     
     def __iter__(self):
         for item in self.previous:
-            for key in item.keys():
-                match = self.keys(key)[1]
-                if match:
-                    dest = self.dest(item, key=key, match=match)
-                    item[dest] = copy.deepcopy(item[key])
-                if self.delete(key)[1]:
-                    del item[key]
+            if self.condition(item):
+                for key in item.keys():
+                    match = self.keys(key)[1]
+                    if match:
+                        dest = self.dest(item, key=key, match=match)
+                        item[dest] = copy.deepcopy(item[key])
+                    if self.delete(key)[1]:
+                        del item[key]
             yield item
