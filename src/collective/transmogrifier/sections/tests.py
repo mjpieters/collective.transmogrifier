300a9f732e39d7304dc44eed9ae74256d7a9171f
Volodymyr Cherepanyak <chervol@gmail.com>
2010-08-20 14:35:48 +0000

    Added recursion to sort sub-dictionaries keys in pprint. To fix quintagroup.transmogrifier tests.

--- src/collective/transmogrifier/sections/tests.py
+++ src/collective/transmogrifier/sections/tests.py
@@ -208,10 +208,27 @@ class PrettyPrinter(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.pprint = pprint.PrettyPrinter().pprint
-    
+            
     def __iter__(self):
+        def undict(source):
+            """ Recurse trough the structure and convert dictionaries 
+                into sorted lists
+            """
+            res = list()
+            if type(source) is dict:
+                source = sorted(source.items())
+            if type(source) in (list, tuple):
+                for item in source:
+                    res.append(undict(item))
+            else:
+                res = source
+            # convert a tuple into tuple back
+            if type(source) is tuple:
+                res = tuple(res)
+            return res
+
         for item in self.previous:
-            self.pprint(sorted(item.items()))
+            self.pprint(undict(item))
             yield item
 
 def sectionsSetUp(test):
