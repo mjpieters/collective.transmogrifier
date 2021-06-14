af71022cf1c76493b3bfbb57f7a0d818b4093f7f
wesleybl <wesleybl@gmail.com>
2021-06-14 11:43:38 -0300

    Use float to define if Plone version is greater than 5.1
    This avoids problems if the first two versions of Plone are made up of
    three digits.

--- src/collective/transmogrifier/__init__.py
+++ src/collective/transmogrifier/__init__.py
@@ -4,6 +4,6 @@ from Products.CMFPlone.utils import getFSVersionTuple
 
 
 version_tuple = getFSVersionTuple()
-PLONE_VERSION = '{0}{1}'.format(version_tuple[0], version_tuple[1])
+PLONE_VERSION = float('{0}.{1}'.format(version_tuple[0], version_tuple[1]))
 
-BIGGER_THEN_PLONE51 = PLONE_VERSION > '51'
+BIGGER_THEN_PLONE51 = PLONE_VERSION > 5.1
