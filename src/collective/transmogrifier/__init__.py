04d0bbfe9957fe23162744a71800663920bdcd97
wesleybl <wesleybl@gmail.com>
2021-06-24 00:42:37 -0300

    Doesn't use unicode in the default value of name of registerConfig  in Plone < 5.2
    This has no longer necessary after we started using DottedName for
    registerConfig name

--- src/collective/transmogrifier/__init__.py
+++ src/collective/transmogrifier/__init__.py
@@ -1,9 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from Products.CMFPlone.utils import getFSVersionTuple
-
-
-version_tuple = getFSVersionTuple()
-PLONE_VERSION = float('{0}.{1}'.format(version_tuple[0], version_tuple[1]))
-
-BIGGER_THEN_PLONE51 = PLONE_VERSION > 5.1
