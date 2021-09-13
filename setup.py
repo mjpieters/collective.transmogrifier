567332fbbfeeee14de3f892710ddafd6cd3d2acb
wesleybl <wesleybl@gmail.com>
2021-09-13 15:19:37 -0300

    isort and Black in setup.py and docs folder

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='collective.transmogrifier',
-    version='1.5.3.dev0',
+    name="collective.transmogrifier",
+    version="1.5.3.dev0",
     description="A configurable pipeline, aimed at transforming content for import and export",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
@@ -34,32 +36,31 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='content import filtering',
-    author='Jarn',
-    author_email='info@jarn.com',
-    url='https://github.com/collective/collective.transmogrifier',
+    keywords="content import filtering",
+    author="Jarn",
+    author_email="info@jarn.com",
+    url="https://github.com/collective/collective.transmogrifier",
     project_urls={
-        'PyPI': 'https://pypi.python.org/pypi/collective.transmogrifier',
-        'Source': 'https://github.com/collective/collective.transmogrifier',
-        'Tracker': 'https://github.com/collective/collective.transmogrifier/issues',
+        "PyPI": "https://pypi.python.org/pypi/collective.transmogrifier",
+        "Source": "https://github.com/collective/collective.transmogrifier",
+        "Tracker": "https://github.com/collective/collective.transmogrifier/issues",
         # 'Documentation': 'https://collective.transmogrifier.readthedocs.io/en/latest/',
     },
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['collective'],
-    package_dir={'': 'src'},
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["collective"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     python_requires="==2.7, >=3.6",
     install_requires=[
-        'Products.CMFCore',
-        'setuptools',
-        'six',
-        'zope.schema',
+        "Products.CMFCore",
+        "setuptools",
+        "six",
+        "zope.schema",
     ],
     extras_require={
-        'test': [
-        ],
+        "test": [],
     },
     entry_points="""
     [z3c.autoinclude.plugin]
