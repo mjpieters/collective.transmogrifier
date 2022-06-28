65c4bc13438780f829d0a426e52b096157e97765
Érico Andrei <ericof@gmail.com>
2022-06-28 19:17:57 -0300

    Refactor package to use Makefile + pip instead of buildout

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "Products.CMFCore",
         "setuptools",
@@ -61,7 +61,10 @@ setup(
         "zope.schema",
     ],
     extras_require={
-        "test": [],
+        "test": [
+            "zest.releaser[recommended]",
+            "zope.testrunner",
+        ]
     },
     entry_points="""
     [z3c.autoinclude.plugin]
